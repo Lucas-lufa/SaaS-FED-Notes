7f02c0470f52faa3a5c1b44c6aecba2d6503612d
Lucas <v092953@tafe.wa.edu.au>
2024-09-14 11:59:43 +0800

    validation, authorise, authorisation

---
+++ 7/Framework/Authorise.php
@@ -0,0 +1,19 @@
+class Authorise 
+{
+    
+    public function isAuthenticated()  
+    {  
+        return Session::has('user');  
+    }
+
+    public function handle($role)  
+    {  
+        if ($role === 'guest' && $this->isAuthenticated()) {  
+            return redirect('/');  
+        }  
+  
+        if ($role === 'auth' && !$this->isAuthenticated()) {  
+            return redirect('/auth/login');  
+        }  
+    }
+}
